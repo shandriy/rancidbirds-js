e0c7d532c1f98f28e14b507f5ba737fde2713d22
entyspite <entyspite@gmail.com>
2024-03-12 21:01:22 -0700

    Add "scenify()" as replacement to "model()"
    scenify, as the name implies, can render a scene (multiple models); Added support for non-triangle polygons in scenify and model functions

--- m.js
+++ m.js
@@ -17,7 +17,8 @@ let camera = {
   fov: 90, screenDistance: window.innerWidth / 2,
   renderDistance: 1000
 }
-let object = [["#ffaa00", { x: -10, y: 0, z: 10 }, { x: 10, y: 0, z: 10 }, { x: 0, y: 10, z: 10 }], ["#ffaabb", { x: -10, y: 0, z: 10 }, { x: 10, y: 0, z: 10 }, { x: 0, y: 10, z: 10 }]];
+let object = [["#ffaa00", { x: -10, y: -10, z: 10 }, { x: 10, y: -10, z: 10 }, { x: 10, y: 10, z: 10 }, { x: -10, y: 10, z: 10 }]];
+let cube = []
 window.onload = function () {
   document.body.innerHTML = "<canvas id='canvas'></canvas>";
   document.getElementById("style").innerHTML = "html,body,canvas{overflow:hidden;margin:0;padding:0;}";
@@ -38,16 +39,68 @@ function run(func) {
 }
 
 function frameR(context) {
-  draw(context, model(context, camera, object));
+  draw(context, scenify(context, camera, [{ model: object, transform: { x: 0, y: 0, z: 0, yaw: 0, pitch: 0, roll: 0 } }]));
 }
 function draw(context, tBatch) {
   context.clearRect(0, 0, context.canvas.width, context.canvas.height);
   //polyBatch(context, [["#ffaa00", [75, 50], [100, 75], [100, 25], [200, 0]], ["#ffaadd", [100, 25], [100, 75], [150, 25]]]);
   polyBatch(context, tBatch);
   //camera.z += frame.delta / 30;
-  //camera.x -= frame.delta / 30;
+  camera.x += frame.delta / 120;
   object[0][3].z -= frame.delta / 90;
 }
+
+function scenify(context, camera, objectArray) {
+  let xd, yd, zd, xt, yt, zt;
+  let fetch, counter, object;
+  let push, bPush, cPush, tPush;
+  let tBatch = [];
+  let width = context.canvas.width / 2;
+  let height = context.canvas.height / 2;
+  for (let c = 0; c < objectArray.length; c++) {
+    object = objectArray[c].transform;
+    xt = object.x;
+    yt = object.y;
+    zt = object.z;
+    object = objectArray[c].model;
+    for (let i = 0; i < object.length; i++) {
+      push = [object[i][0]];
+      bPush = object[i].length - 1;
+      cPush = bPush;
+      for (let j = 1; j < object[i].length; j++) {
+        fetch = object[i][j];
+        xd = fetch.x + xt - camera.x;
+        yd = fetch.y + yt - camera.y;
+        zd = fetch.z + zt - camera.z;
+        if (Math.pow(camera.renderDistance, 2) <= (xd * xd) + (yd * yd) + (zd * zd)) bPush--;
+        zd = camera.screenDistance / zd;
+        xd *= zd;
+        yd *= zd;
+        if (Math.abs(xd) > width + 2 || Math.abs(yd) > height + 2) cPush--;
+        if (zd <= 0) {
+          yd *= -1;
+          xd *= -1;
+        }
+        push.push([Math.floor(width + xd), Math.floor(height - yd)]);
+      }
+      for (let j = 1; j < object[i].length; j++) {
+        tPush = push[j];
+        counter = 0;
+        for (let k = 1; k < object[i].length; k++) {
+          if (push[k][0] == tPush[0] && push[k][1] == tPush[1]) counter++;
+        }
+        if (counter != 1) {
+          bPush = -1;
+          cPush = -1;
+          break;
+        }
+      }
+      if (bPush > 0 && cPush > 0) tBatch.push(push);
+    }
+  }
+  return tBatch;
+}
+
 function model(context, camera, object) {
   let xd, yd, zd, fetch, push, bPush, cPush, tPush, counter;
   let tBatch = [];
@@ -57,7 +110,7 @@ function model(context, camera, object) {
     push = [object[i][0]];
     bPush = 3;
     cPush = 3;
-    for (let j = 1; j < 4; j++) {
+    for (let j = 1; j < object[i].length; j++) {
       fetch = object[i][j];
       xd = fetch.x - camera.x;
       yd = fetch.y - camera.y;
@@ -67,13 +120,16 @@ function model(context, camera, object) {
       xd *= zd;
       yd *= zd;
       if (Math.abs(xd) > width + 2 || Math.abs(yd) > height + 2) cPush--;
-      if (zd <= 0) yd *= -1;
+      if (zd <= 0) {
+        yd *= -1;
+        xd *= -1;
+      }
       push.push([Math.floor(width + xd), Math.floor(height - yd)]);
     }
-    for (let j = 1; j < 4; j++) {
+    for (let j = 1; j < object[i].length; j++) {
       tPush = push[j];
       counter = 0;
-      for (let k = 1; k < 4; k++) {
+      for (let k = 1; k < object[i].length; k++) {
         if (push[k][0] == tPush[0] && push[k][1] == tPush[1]) counter++;
       }
       if (counter != 1) {
