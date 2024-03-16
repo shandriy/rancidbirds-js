6b6028db93bdca8fd3ea8b8a0f330b5bb932733f
entyspite <entyspite@gmail.com>
2024-03-16 16:05:46 -0700

    undo triangle functions
    for new build (camera)

--- proj/deps/exec.js
+++ proj/deps/exec.js
@@ -14,7 +14,7 @@ function frameR(context) {
   processInput();
 
   // find draw in render.js, and scenify in project.js (project as in projection)
-  draw(context, scenifyTriangle(context, camera, [{ model: cube, transform: { x: 0, y: 0, z: 0, yaw: 0, pitch: 0, roll: 0 } }]));
+  draw(context, scenify(context, camera, [{ model: cube, transform: { x: 0, y: 0, z: 0, yaw: 0, pitch: 0, roll: 0 } }]));
 }
 
 
