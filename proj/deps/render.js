0ed573ace37da3884cc760811c4e2141bd338a17
entyspite <entyspite@gmail.com>
2024-03-16 22:40:22 -0700

    Added triFillTop() function
    does pretty much the same thing as triFillBottom but now (with a bit more work) all be able to draw triangles of any shape

--- proj/deps/render.js
+++ proj/deps/render.js
@@ -99,7 +99,42 @@ function triangleBatchPixel(context, triangleArray) {
         currentX2 += slope2;
       }
     }
-    triFillBottom(yIndexer);
+    function triFillTop(tri) {
+      let slope1 = (tri[2][0] - tri[0][0]) / (tri[2][1] - tri[0][1]);
+      let slope2 = (tri[2][0] - tri[1][0]) / (tri[2][1] - tri[1][1]);
+      let is1 = false;
+      slope1 < slope2 ? is1 = false : is1 = true;
+      let currentX1 = tri[2][0];
+      let currentX2 = currentX1;
+      let inCycle;
+      for (let y = tri[2][1]; y > tri[1][1]; y--) {
+        if (is1) {
+          for (let x = Math.floor(currentX1); x <= Math.ceil(currentX2); x++) {
+            inCycle = ((width * y) + x) * 4;
+            if (x % width == x && x >= 0) {
+              for (let j = 0; j < 3; j++) {
+                imgData[inCycle + j] = rgb[j];
+              }
+              imgData[inCycle + 3] = 255;
+            }
+          }
+        }
+        else {
+          for (let x = Math.floor(currentX2); x <= Math.ceil(currentX1); x++) {
+            inCycle = ((width * y) + x) * 4;
+            if (x % width == x && x >= 0) {
+              for (let j = 0; j < 3; j++) {
+                imgData[inCycle + j] = rgb[j];
+              }
+              imgData[inCycle + 3] = 255;
+            }
+          }
+        }
+        currentX1 -= slope1;
+        currentX2 -= slope2;
+      }
+    }
+    triFillTop(yIndexer);
     /*if (polyArray[i].length >= 4) {
       context.beginPath();
       context.moveTo(polyArray[i][1][0], polyArray[i][1][1]);
