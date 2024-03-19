b9b5fdbf902cad04d4a990254bdb9cfb7f8b63ad
Konstantin Edunov <151408521+shandriy@users.noreply.github.com>
2024-03-19 13:45:21 -0700

    Updated -locket to avoid errors
    a lot of it is unnecessary but it's still better than before (IMO)

--- lang/-locket.js
+++ lang/-locket.js
@@ -1,16 +1,31 @@
 "use strict";
 
+let uniLoc = "EN";
 function setLangPrefs() {
+  let tCounter = 0;
   let lang = window.navigator.language.substring(0, 2).toUpperCase();
-  switch (lang) {
-    case "EN":
-      // do nothing. These translations will only matter later
-      break;
-    case "RU":
-      document.getElementsByTagName("title")[0].innerHTML = "ПРОГОРКЛЫЕ-ПТИЦЫ";
-      break;
-    default:
-      break;
+  uniLoc = lang;
+  let lcr
+  function langSwitch(loc) {
+    tCounter++;
+    switch (lang) {
+      case "EN":
+        // do nothing. These translations will only matter later
+        break;
+      case "RU":
+        document.getElementsByTagName("title")[0].innerHTML = "ПРОГОРКЛЫЕ-ПТИЦЫ";
+        break;
+      default:
+        lang = "EN"
+        if (counter <= 5)
+        {
+          langSwitch("EN")
+        }
+        break;
+      return lang;
+    }
+  langSwitch(lang);
+  uniLoc = lang;
   }
 }
 
