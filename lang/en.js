636a7d319e2096bd1865def12b541e9c78b9f442
Konstantin Edunov <151408521+shandriy@users.noreply.github.com>
2024-03-19 13:57:38 -0700

    Created en.js file
    contains common dialogues/messages for the game in english; I'll add other localizations after

---
+++ lang/en.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let fullLocals = {
+  exit: "EXIT",
+  lab1: "A0 : HALL",
+  lab2: "A1 : LAB",
+  lab3: "B0 : DORM",
+  elevator: "ELEVATOR",
+  elevatorMsg: "CAUTION : MOVING DOORS",
+  caution: "CAUTION",
+  employees: "KEEP OUT : EMPLOYEES ONLY",
+  danger: "WARNING : PROCEED AT YOUR OWN RISK; WE DO NOT TAKE ANY RESPONSIBILITY FOR DAMAGES CAUSED BEYOND THIS POINT; RISK MAY INCLUDE BUT IS NOT LIMITED TO : DEATH"
+}
+
+/// end
+lc++
